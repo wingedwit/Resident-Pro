4db97d7832fa96245b44558b7dd64795664ebedf
wingedwit <vaibhav.ganesh51@gmail.com>
2026-09-13 19:39:01 +0530

    Match Resident-Pro title styling to Attendance-Pro

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '1.0.7';
+const APP_VERSION = '1.0.8';
 const CACHE_VERSION = `resident-pro-v${APP_VERSION}`;
 const SHELL_CACHE = `shell-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `runtime-${CACHE_VERSION}`;
